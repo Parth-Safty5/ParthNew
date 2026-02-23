885c7bd8ea50aaefc21a8c2dea1922380665b50f
Parth-Safty5 <parthsafty5@gmail.com>
2026-02-23 20:47:14 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'parthnew-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
