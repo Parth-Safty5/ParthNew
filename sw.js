faaf8967ba37c4c31a64f3afaa0ec0fc33ecd2f4
Parth-Safty5 <parthsafty5@gmail.com>
2026-03-01 12:25:05 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10668069
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+
 const cacheName = 'parthnew-v3'; // Is baar v3 kar dete hain, iske baad tension khatam
 
 // Sirf shuruati zaroori files
