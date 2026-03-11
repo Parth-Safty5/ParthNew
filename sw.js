6f0ae4e0d35a9d1fedec629b2c75a44707989bb2
Parth-Safty5 <parthsafty5@gmail.com>
2026-03-11 08:39:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,5 +3,4 @@ const coreAssets=['./','./index.html','./manifest.json','./logo.webp','./load-co
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(coreAssets)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==cacheName).map(key=>caches.delete(key)))));});
-
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).then(networkResponse=>{if(networkResponse&&networkResponse.status===200){const responseToCache=networkResponse.clone();caches.open(cacheName).then(cache=>cache.put(event.request,responseToCache));}return networkResponse;}).catch(()=>caches.match(event.request)));});
